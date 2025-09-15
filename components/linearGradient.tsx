4da9a019683a0c199b4a14b5bb4815011f242276
omormehn <nathanuseni@gmail.com>
2025-09-15 10:46:43 +0100

    removed duplicate type

--- components/linearGradient.tsx
+++ components/linearGradient.tsx
@@ -1,22 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { LinearGradient, LinearGradientPoint } from 'expo-linear-gradient'
+import { LinearGradient, type LinearGradientPoint } from 'expo-linear-gradient'
 import { useTheme } from '@/context/ThemeContext'
+import type { StyleProp, ViewStyle } from 'react-native';
+
+
 interface LinearGradientProps {
-    x1?: number;
-    y1?: number;
-    x2?: number;
-    y2?: number;
-    styles: object;
+    start?: LinearGradientPoint;
+    end?: LinearGradientPoint;
+    styles: StyleProp<ViewStyle>;
     children?: React.ReactNode;
 }
-const LinearGradientComponent = ({ x1, x2, y1, y2, styles, children }: LinearGradientProps) => {
+const LinearGradientComponent = ({ start = { x: 0, y: 0 }, end = { x: 1, y: 1 }, styles, children }: LinearGradientProps) => {
     const { gradients } = useTheme();
     return (
         <LinearGradient
             colors={gradients.card}
-            start={{ x: x1!, y: y1! }}
-            end={{ x: x2!, y: y2! }}
+            start={start}
+            end={end}
             style={styles}
         >
             {children}
