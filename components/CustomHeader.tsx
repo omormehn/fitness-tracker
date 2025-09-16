b688d93732d2b412952173e2544a9375268ed267
omormehn <nathanuseni@gmail.com>
2025-09-16 01:01:31 +0100

    added notification screen header

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,26 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import Entypo from '@expo/vector-icons/Entypo';
 import { useTheme } from '@/context/ThemeContext';
+import { router } from 'expo-router';
 
 
 const CustomHeader = ({ title }: { title: string }) => {
-    const { theme } = useTheme();
+    const { theme, colors } = useTheme();
     const color = theme === 'dark' ? 'white' : 'black'
     return (
-        <View>
+        <View style={styles.container}>
             {/* Icon 1 */}
-            <View>
+            <TouchableOpacity onPress={() => router.back()} style={[styles.iconBg, { backgroundColor: colors.iconBg, }]}>
                 <MaterialIcons name="arrow-left" size={24} color={color} />
-            </View>
+            </TouchableOpacity>
             {/* Title  */}
             <View>
-                <Text>{title}</Text>
+                <Text style={[{ fontFamily: 'PoppinsBold', fontSize: 16 }, { color: colors.text }]}>{title}</Text>
             </View>
             {/* Icon 3 */}
-            <View>
-                <Entypo name="dots-two-horizontal" size={24} color="black" />
+            <View style={[styles.iconBg, { backgroundColor: colors.iconBg, }]}>
+                <Entypo name="dots-two-horizontal" size={24} color={color} />
             </View>
         </View>
     )
@@ -30,9 +31,11 @@ export default CustomHeader
 
 const styles = StyleSheet.create({
     container: {
+        width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         gap: 30
-    }
+    },
+    iconBg: { borderRadius: 10, paddingHorizontal: 4, paddingVertical: 4 }
 })
